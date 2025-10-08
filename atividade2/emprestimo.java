4c5b0ab8b0d6078f3f6cf49eed13637059b1085d
lauramavalli01 <lauramavalli01@gmail.com>
2025-10-08 12:10:52 -0300

    atividades

---
+++ atividade2/emprestimo.java
@@ -0,0 +1,43 @@
+package atividade2;
+
+public class emprestimo {
+    private Pessoa pessoa;
+    private Material material;
+    private String dataEmprestimo;
+    private String dataDevolucao;
+
+    public emprestimo (Pessoa pessoa, Material material, String dataEmprestimo, String dataDevolucao){
+        this.pessoa = pessoa;
+        this.material = material;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Pessoa getPessoa(){
+        return this.pessoa;
+    }
+
+    public Material material(){
+        return this.material;
+    }
+
+    public String dataEmprestimo(){
+        return this.dataEmprestimo;
+    }
+
+    public String dataDevolucao(){
+        return this.dataDevolucao;
+    }
+
+    public void exibirInfo(){
+        System.out.println("data de emprestimo:" + this.dataEmprestimo);
+        System.out.println("data de devolucao:" + this.dataDevolucao);
+        System.out.println("pessoa vinculada:");
+        this.pessoa.exibirInfo();
+        System.out.println("material vinculado:");
+        this.material.descricao();
+    }
+    
+    
+    
+}
