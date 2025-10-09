7165d78a7c00fa4f31e0a0388a4d07755ed26b33
lauramavalli01-del <lauramavalli01@gmail.com>
2025-10-08 21:40:12 -0300

    Create Revista.java

---
+++ atividade2/Revista.java
@@ -0,0 +1,19 @@
+public class Revista extends Material {
+    private String autor;
+    
+    public Revista(String titulo, int anoPublicacao, String autor) {
+        super(titulo, anoPublicacao);
+        this.autor = autor;
+    }
+    
+    @Override
+    public void descricao() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Ano de Publicação: " + anoPublicacao);
+        System.out.println("Autor: " + autor);
+    }
+    
+    public String getAutor() {
+        return autor;
+    }
+}
