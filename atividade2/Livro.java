d65f1032112ee610179e6939cdb1b137d779b8f6
lauramavalli01-del <lauramavalli01@gmail.com>
2025-10-08 21:38:57 -0300

    Update Livro.java

--- atividade2/Livro.java
+++ atividade2/Livro.java
@@ -1,13 +1,21 @@
 package atividade2;
 
-public class Livro extends Material{
-    private String autor;
-
-    public 
-
-    public String getAutor(){
-        return this.autor;
+public class Livro extends Material {
+    private String edicao;
+    
+    public Livro(String titulo, int anoPublicacao, String edicao) {
+        super(titulo, anoPublicacao);
+        this.edicao = edicao;
     }
-
-    public void setAutor(S)
+    
+    @Override
+    public void descricao() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Ano de Publicação: " + anoPublicacao);
+        System.out.println("Edição: " + edicao);
+    }
+    
+    public String getEdicao() {
+        return edicao;
+    }
 }
